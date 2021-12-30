406caf5d28cce79c9ef73737651ff7af1f7c23e2
SB Components <gajender.singh@sb-components.co.uk>
2021-12-30 11:24:57 +0530

    Update Send_data_Thingspeak.py

--- Send_data_Thingspeak.py
+++ Send_data_Thingspeak.py
@@ -33,7 +33,7 @@ WIDTH  = 128                                            # oled display width
 HEIGHT = 32                                             # oled display height
 
 #i2c = I2C(0)                                            # Init I2C using I2C0 defaults, SCL=Pin(GP9), SDA=Pin(GP8), freq=400000
-i2c = I2C(0, freq=399361, scl=Pin(21), sda=Pin(20))
+i2c = I2C(0, sda=Pin(20), scl=Pin(21))
 print(i2c)
 print("I2C Address      : "+hex(i2c.scan()[0]).upper()) # Display device address
 print("I2C Configuration: "+str(i2c))                   # Display I2C config
